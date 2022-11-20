6e95b32c2598b05351e08df2e0226a5748b5836f
gusstvocardoso@gmail.com <gusstavocardoso@gmail.com>
2022-11-20 20:44:44 -0300

    teste sonar

--- tests/data.test.js
+++ tests/data.test.js
@@ -2,7 +2,7 @@ const data = require("../src/data");
 
 describe('Objetos', () => {
   test("Deve aribuir valor a um objeto", () => {
-    expect(data).toEqual({ one: 1, two: 2 });
+    expect(data).toEqual({ one: 1, two: 1 });
   });
   
 });
