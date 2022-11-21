a317c2cadd83057f458cf298086626b4f23b9d33
gusstvocardoso@gmail.com <gusstavocardoso@gmail.com>
2022-11-20 21:41:07 -0300

    add test multiplica

--- tests/calculo.test.js
+++ tests/calculo.test.js
@@ -1,7 +1,12 @@
 const soma = require("../src/calculo");
+const multiplica = require("../src/calculo");
 
 describe("Calculos", () => {
   test("Deve somar dois valores", () => {
     expect(soma(1, 2)).toBe(3);
+  });
+
+  test("Deve multiplicar dois valores", () => {
+    expect(multiplica(2, 2)).toBe(4);
   });
 });
